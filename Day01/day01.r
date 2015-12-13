a3e2faede734260d3006cf7a0ba562bcf69fa85b
Scott Shepard <scott@shepardmail.com>
2015-12-13 17:35:03 -0600

    part 2 of day 1

--- Day01/day01.r
+++ Day01/day01.r
@@ -7,5 +7,10 @@ txt_vec[txt_vec == ")"] <- "-1"
 
 txt_vec <- as.numeric(txt_vec)
 
+# Part 1
 sum(txt_vec) 
-# Answer is 232
+# Answer to Part 1 is 232
+
+# Part 2
+which(cumsum(txt_vec) == -1)[1]
+# Answer to Part 2 is 1783
