9ee26d6ba74914ed4bb6a32b4cfee899885db0f5
Scott Shepard <scott@shepardmail.com>
2015-12-13 20:45:57 -0600

    day 3 part 2 solution

--- Day03/day_03.r
+++ Day03/day_03.r
@@ -1,9 +1,10 @@
 library(ggplot2)
-library(plyr)
+library(dplyr)
 
 houses <- readLines("data.txt")
 houses <- as.list(strsplit(houses, "")[[1]])
 
+# Part 1
 directionVector <- function(direction) {
   if(direction == "v") {
     c(x=0, y=-1)
@@ -18,7 +19,7 @@ directionVector <- function(direction) {
   }
 }
 
-direction_matrix <- ldply(houses, directionVector)
+direction_matrix <- plyr::ldply(houses, directionVector)
 direction_matrix$X <- cumsum(direction_matrix$x)
 direction_matrix$Y <- cumsum(direction_matrix$y)
 
@@ -32,4 +33,19 @@ ggplot(direction_matrix, aes(x=X, y=Y, color=color)) + geom_point() +
   theme(axis.title=element_blank(), legend.position="none") +
   ggtitle("Map of Houses Santa Visits")
 
+# Part 2
+direction_matrix$santa <- c("Santa", "Robo-Santa")
+santa <- filter(direction_matrix, santa == "Santa")
+robosanta <- filter(direction_matrix, santa == "Robo-Santa")
+santa$X <- cumsum(santa$x)
+santa$Y <- cumsum(santa$y)
+robosanta$X <- cumsum(robosanta$x)
+robosanta$Y <- cumsum(robosanta$y)
+santas_matrix <- rbind(santa, robosanta)
 
+sum(! duplicated(santas_matrix[, c("X", "Y")]))
+
+ggplot(santas_matrix, aes(x=X, y=Y, color=santa)) + geom_point() +
+  scale_color_manual(values=c("#d42426", "#3C8D0D")) +
+  theme(axis.title=element_blank()) +
+  ggtitle("Map of Houses Santa & Robo-Santa Visits")
