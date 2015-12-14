ea460732996c39c4fb716e150db5e317197b97ea
Scott Shepard <scott@shepardmail.com>
2015-12-13 20:30:39 -0600

    Day 2 part 2 solution

--- Day02/day_02.r
+++ Day02/day_02.r
@@ -1,5 +1,6 @@
 presents <- readLines("data.txt")
 
+# Part 1
 wrappingPaperNeeded <- function(l, w, h) {
   smallest_side <- min(c(l*w, w*h, h*l))
   2*l*w + 2*w*h + 2*h*l + smallest_side
@@ -14,3 +15,17 @@ wrapping_area <- sapply(presents, function(present) {
 
 sum(wrapping_area)
 # Answer is 1588178 square feet of wrapping paper
+
+# Part 2
+ripponNeeded <- function(l, w, h) {
+  smallest_two_sides <- c(l, w, h)[order(c(l, w, h))][c(1,2)]
+  sum(smallest_two_sides * 2) + l*w*h
+}
+
+ribbon_length <- sapply(presents, function(present) {
+  pdims <- as.numeric(present)
+  ripponNeeded(pdims[1], pdims[2], pdims[3])
+})
+
+sum(ribbon_length)
+# Answer is 3783758 feet of ribbon
