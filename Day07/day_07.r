371c2ba0c6748d1cfc5b4e4255023d36a60d458f
Scott Shepard <scott@shepardmail.com>
2015-12-17 22:35:43 -0600

    Save values of recursive tree

--- Day07/day_07.r
+++ Day07/day_07.r
@@ -1,19 +1,30 @@
 library(stringr)
 
 CIRCUITS <- readLines("data.txt")
+KNOWNVALUES = data.frame()
 
 findValue <- function(circuit_code, circuits = CIRCUITS) {
-  # TODO complete this function
-  assignment_circuit <- findAssignmentCircuit(circuit_code, circuits)
-  dependencies <- findDependentCircuits(assignment_circuit)
-  if(is.null(dependencies)) {
-    print(paste("Evaluating", circuit_code))
-    evaluateValue(assignment_circuit)
+  # Recursive function to find the value of a given circuit code
+  
+  # First check to see if that value has been evaluated before and 
+  # return if it has. This will prevent reevaluating the same code 
+  # and thus prevent long runtimes
+  if(circuit_code %in% KNOWNVALUES$code) {
+    return(KNOWNVALUES[KNOWNVALUES$code == circuit_code, ]$value)
   } else {
-    dependent_vals <- sapply(dependencies, findValue, circuits)
-    assignment_circuit_new <- str_replace_all(assignment_circuit, dependent_vals)
-    circuits <- str_replace(circuits, assignment_circuit, assignment_circuit_new)
-    findValue(circuit_code, circuits)
+    assignment_circuit <- findAssignmentCircuit(circuit_code, circuits)
+    dependencies <- findDependentCircuits(assignment_circuit)
+    if(is.null(dependencies)) {
+      print(paste("Evaluating", circuit_code))
+      ans <- evaluateValue(assignment_circuit)
+      KNOWNVALUES <<- rbind(KNOWNVALUES, data.frame(code = circuit_code, value = ans))
+      ans
+    } else {
+      dependent_vals <- sapply(dependencies, findValue, circuits)
+      assignment_circuit_new <- str_replace_all(assignment_circuit, dependent_vals)
+      circuits <- str_replace(circuits, assignment_circuit, assignment_circuit_new)
+      findValue(circuit_code, circuits)
+    }
   }
 }
 
@@ -37,30 +48,34 @@ evaluateValue <- function(ac) {
 ### Cases
   valcode <- str_extract(ac, "[a-z]+$")
   operator <- str_extract(ac, "[A-Z]+")
-  ans <- if(is.na(operator)) {
-    as.numeric(str_extract(ac, "^([:digit:])+"))
+  if(is.na(operator)) {
+    as.numeric(str_extract(ac, "^(-?[:digit:])+"))
   } else if(operator == "NOT") {
-    bitwNot(as.numeric(str_extract(ac, "(?<=NOT )[:digit:]+")))
+    bitwNot(as.numeric(str_extract(ac, "(?<=NOT )-?[:digit:]+")))
   } else if(operator == "AND") {
-    
+    a <- as.numeric(str_extract(ac, "-?[:digit:]+(?= AND)"))
+    b <- as.numeric(str_extract(ac, "(?<=AND )-?[:digit:]+"))
+    bitwAnd(a, b)
   } else if(operator == "OR") {
-    
+    a <- as.numeric(str_extract(ac, "-?[:digit:]+(?= OR)"))
+    b <- as.numeric(str_extract(ac, "(?<=OR )-?[:digit:]+"))
+    bitwOr(a, b)
   } else if(operator == "LSHIFT") {
-    
+    a <- as.numeric(str_extract(ac, "^(-?[:digit:])+"))
+    n <- as.numeric(str_extract(ac, "(?<=LSHIFT )-?[:digit:]+"))
+    bitwShiftL(a, n)
   } else if(operator == "RSHIFT") {
-    a <- as.numeric(str_extract(ac, "^([:digit:])+"))
-    n <- as.numeric(str_extract(ac, "(?<=RSHIFT )[:digit:]+"))
+    a <- as.numeric(str_extract(ac, "^(-?[:digit:])+"))
+    n <- as.numeric(str_extract(ac, "(?<=RSHIFT )-?[:digit:]+"))
     bitwShiftR(a, n)
   }
-  # NOT
-    # only one value
-  # AND 
-    # always two values
-  # OR
-    # always two values
-  # LSHIFT
-    # only one value & shift num
-  # RSHIFT
-    # only one value & shift num
-  ans
 }
+
+# Part 1
+findValue("a")
+# Answer is 3176
+
+# Part 2
+# Change data.txt so that 3176 -> b 
+# Then rerun
+findValue("a")
