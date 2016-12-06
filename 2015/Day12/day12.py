36b3967e9d6fed2ddc03a53951c447b1c78adedd
Scott Shepard <scott@shepardmail.com>
2016-12-06 17:28:31 -0600

    Day 12, 2015 part 2 in python

--- 2015/Day12/day12.py
+++ 2015/Day12/day12.py
@@ -19,17 +19,70 @@
 #
 # What is the sum of all numbers in the document?
 #
+# --- Part Two ---
+#
+# Uh oh - the Accounting-Elves have realized that they double-counted
+# everything red.
+#
+# Ignore any object (and all of its children) which has any property with the 
+# value "red". Do this only for objects ({...}), not arrays ([...]).
+#
+# [1,2,3] still has a sum of 6.
+# [1,{"c":"red","b":2},3] now has a sum of 4, 
+# because the middle object is ignored.
+# {"d":"red","e":[1,2,3,4],"f":5} now has a sum of 0, 
+# because the entire structure is ignored.
+# [1,"red",5] has a sum of 6, because "red" in an array has no effect.
+# 
 # -----------------------------------------------------------------------------
 
 import json
 import re
 
+class Day12:
+
+    def __init__(self, data):
+        self.data = data
+        self.json = json.loads(data)
+        self.no_red_sum = 0
+
+    def sum_all_numbers(self):
+        numbers = re.findall('-?[0-9]*', self.data)
+        return sum([int(n) for n in numbers if n != ''])
+
+    def sum_nonred(self, json):
+        # Base case
+        if(type(json) is int):
+            return json
+        elif(type(json) is str):
+            return 0
+        # If list, iterate through list and get the sum 
+        # of each item protecting for None
+        elif(type(json) is list):
+            result = 0
+            for item in json:
+                summand = self.sum_nonred(item)
+                if(summand is not None):
+                    result += summand
+            return result
+        # If dict, check first for a 'red' value, if
+        # not pass back into recurrsion and let 
+        # the list step handle it
+        elif(type(json) is dict):
+            values = list(json.values())
+            if('red' in values):
+                return 0
+            else:
+                return self.sum_nonred(values)
+
 if __name__ == '__main__':
     fileobject = open('day12.txt', 'r')
     data = fileobject.read()
 
-    numbers = re.findall('-?[0-9]*', data)
-    print("Part 1:", sum([int(n) for n in numbers if n != '']))
+    day12 = Day12(data)
+
+    print("Part 1:", day12.sum_all_numbers())
     # Correct answer is 191164
 
-
+    print("Part 2:", day12.sum_nonred(day12.json))
+    # Correct answer is 87842
