caca2c7aa19d3a3f06413630198c32562ce875f2
Scott Shepard <scott@shepardmail.com>
2016-12-07 13:15:24 -0600

    Add into comment text

--- 2016/day07.py
+++ 2016/day07.py
@@ -1,3 +1,52 @@
+# --- Day 7: Internet Protocol Version 7 ---
+#
+# While snooping around the local network of EBHQ, you compile a list of IP 
+# addresses (they're IPv7, of course; IPv6 is much too limited). You'd like to 
+# figure out which IPs support TLS (transport-layer snooping).
+#
+# An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA.
+# An ABBA is any four-character sequence which consists of a pair of two 
+# different characters followed by the reverse of that pair, such as xyyx or 
+# abba. However, the IP also must not have an ABBA within any hypernet 
+# sequences, which are contained by square brackets.
+#
+# For example:
+#
+# abba[mnop]qrst supports TLS (abba outside square brackets).
+# abcd[bddb]xyyx does not support TLS (bddb is within square brackets, 
+# even though xyyx is outside square brackets).
+# aaaa[qwer]tyui does not support TLS (aaaa is invalid; the interior characters
+# must be different).
+# ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even 
+# though it's within a larger string).
+# How many IPs in your puzzle input support TLS?
+#
+# --- Part Two ---
+#
+# You would also like to know which IPs support SSL (super-secret listening).
+#
+# An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere in 
+# the supernet sequences (outside any square bracketed sections), and a 
+# corresponding Byte Allocation Block, or BAB, anywhere in the hypernet 
+# sequences. An ABA is any three-character sequence which consists of the same 
+# character twice with a different character between them, such as xyx or aba. 
+# A corresponding BAB is the same characters but in reversed positions: yxy and
+# bab, respectively.
+#
+# For example:
+#
+# aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab 
+# within square brackets).
+# xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+# aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet;
+# the aaa sequence is not related, because the interior character must be 
+# different).
+# zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a 
+# corresponding bzb, even though zaz and zbz overlap).
+# How many IPs in your puzzle input support SSL?
+#
+# -----------------------------------------------------------------------------
+
 import re
 
 def supports_tls(string):
@@ -6,6 +55,19 @@ def supports_tls(string):
     a = detect_abba_in_list(nhtxt)
     b = detect_abba_in_list(htxt)
     return (a and not b)
+
+def nonhypernet_text(string):
+    if re.search('\[', string) is None:
+        return string
+    between_htxt = re.findall(r"\]([A-Za-z0-9_]+)\[", string)
+    before_htxt = re.findall(r"([A-Za-z0-9_]+)\[", string)
+    after_htxt = re.findall(r"]([A-Za-z0-9_]+)", string)
+    return between_htxt + before_htxt + after_htxt
+
+def hypernet_text(string):
+    '''Return a list of all text found between square brackets
+       i.e. [xxyy]asdjfkl[aabbaa] => ['xxyy', 'aabbaa']'''
+    return re.findall(r"\[([A-Za-z0-9_]+)\]", string)
 
 def supports_ssl(string):
     nhtxt = nonhypernet_text(string)
@@ -54,9 +116,6 @@ def aba(string):
 def bab(string, aba):
     return string == (aba[1] + aba[0] + aba[1])
 
-def hypernet_text(string):
-    return re.findall(r"\[([A-Za-z0-9_]+)\]", string)
-
 def all_nletter_seqs(string, n):
     if len(string) < n:
         return []
@@ -66,14 +125,6 @@ def all_nletter_seqs(string, n):
         for i in range(0, len(chars)-(n-1)):
             sets.append(''.join(chars[i:i+n]))
         return sets
-
-def nonhypernet_text(string):
-    if re.search('\[', string) is None:
-        return string
-    between_htxt = re.findall(r"\]([A-Za-z0-9_]+)\[", string)
-    before_htxt = re.findall(r"([A-Za-z0-9_]+)\[", string)
-    after_htxt = re.findall(r"]([A-Za-z0-9_]+)", string)
-    return between_htxt + before_htxt + after_htxt
 
 if __name__ == '__main__':
     fileobject = open('inputs/day07.txt')
