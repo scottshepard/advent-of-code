8dbd2c7a15d9a0f9d3e9f0c3c5f03f86bc89a23b
Scott Shepard <scott@shepardmail.com>
2016-12-08 15:40:03 -0600

    Day 8 parts 1 & 2

--- 2016/day08.py
+++ 2016/day08.py
@@ -63,3 +63,87 @@
 #
 # -----------------------------------------------------------------------------
 
+import re
+
+class Screen:
+
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+        self.screen = [[0 for x in range(w)] for y in range(h)]
+
+    def __repr__(self):
+        for row in self.screen:
+            arr = ['' for i in range(len(row))]
+            for i in range(len(row)):
+                if (i+1) % 5 == 0 and i != 0:
+                    arr[i] = ' | '
+                elif row[i] == 1:
+                    arr[i] = '0'
+                else:
+                    arr[i] = ' '
+            print(''.join(arr))
+        return ''
+
+    def rect(self, A, B):
+        for j in range(B):
+            for i in range(A):
+                self.screen[j][i] = 1
+
+    def rotate_row(self, y, num):
+        row = self.screen[y]
+        new_row = [0 for x in range(len(row))]
+        for i in range(len(row)):
+            new_row[(i+num) % len(row)] = row[i]
+        self.screen[y] = new_row
+
+    def rotate_column(self, x, num):
+        screen = self.screen
+        n_rows = len(screen)
+        col = [screen[j][x] for j in range(n_rows)]
+        new_col = [0 for y in range(n_rows)]
+        for j in range(n_rows):
+            screen[(j+num) % n_rows][x] = col[j]
+        self.screen = screen
+
+    def sum(self):
+        screen = self.screen
+        w = self.width
+        h = self.height
+        return sum([sum([screen[j][i] for i in range(w)]) for j in range(h)])
+
+class Instruction:
+
+    def __init__(self, raw_text):
+        self.raw = raw_text
+
+    def parse(self):
+        text = self.raw
+        function = re.search('rect|row|column', text).group(0)
+        if(function == 'rect'):
+            A = int(re.search('[0-9]+(?=x)', text).group(0))
+            B = int(re.search('(?<=x)[0-9]+', text).group(0))
+            return function, A, B
+        else:
+            xy = int(re.search('(?<=\=)[0-9]+', text).group(0))
+            n = int(re.search('[0-9]+$', text).group(0))
+            return function, xy, n
+
+if __name__ == '__main__':
+    fileobject = open('inputs/day08.txt')
+    data = fileobject.read()
+    lines = re.split('\n', data)
+    s = Screen(50, 6)
+    instructions = [Instruction(l).parse() for l in lines]
+    for inst in instructions:
+        if(inst[0] == 'rect'):
+            s.rect(inst[1], inst[2])
+        elif(inst[0] == 'row'):
+            s.rotate_row(inst[1], inst[2])
+        elif(inst[0] == 'column'):
+            s.rotate_column(inst[1], inst[2])
+    print("Part 1:", s.sum())
+    # Correct answer is 121
+    print("Part 2:")
+    print(s)
+    # Correct answer is RURUCEOEIL
