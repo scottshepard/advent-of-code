51ea2a2a09cf118c3b75d5d0a1fd62977384fff9
Scott Shepard <scott@shepardmail.com>
2016-12-10 17:22:04 -0600

    Part 2

--- 2016/day09.py
+++ 2016/day09.py
@@ -66,9 +66,30 @@ def decompress(text):
     remaining = text[(end+char_len):]
     return pre_match + decompressed_section + decompress(remaining)
 
+def calculate_decompress_len(text):
+    search = re.search('\([0-9]+x[0-9]+\)', text)
+    if search is None:
+        return len(text)
+    start = search.start()
+    end = search.end()
+    pre_match_len = len(text[:start])
+    match = search.group(0)
+    char_len = int(re.search('[0-9]+(?=x)', match).group(0))
+    repeats = int(re.search('(?<=x)[0-9]+', match).group(0))
+    matched_chars = text[end:(end+char_len)]
+    search_matched = re.search('\([0-9]+x[0-9]+\)', matched_chars)
+    if search_matched is None:
+        matched_len = char_len * repeats
+    else:
+        matched_len = calculate_decompress_len(matched_chars) * repeats
+    remaining = text[(end+char_len):]
+    return pre_match_len + matched_len + calculate_decompress_len(remaining)
+
 if __name__ == '__main__':
     fileobject = open('inputs/day09.txt')
     data = fileobject.read()
     lines = re.split('\n', data)
     print(sum([len(decompress(line)) for line in lines]))
     # Correct answer is 110346
+    print(sum([calculate_decompress_len(line) for line in lines]))
+    # Correct answer is 10774309173
