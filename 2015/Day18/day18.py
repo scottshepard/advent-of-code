94b61123dca73c12d950fee4eb4da9bd719bebb9
Scott Shepard <scott@shepardmail.com>
2016-12-11 00:11:44 -0600

    Instructions for part 2

--- 2015/Day18/day18.py
+++ 2015/Day18/day18.py
@@ -84,6 +84,75 @@
 # In your grid of 100x100 lights, given your initial configuration, 
 # how many lights are on after 100 steps?
 #
+#
+# --- Part Two ---
+#
+# You flip the instructions over; Santa goes on to point out that this is all 
+# just an implementation of Conway's Game of Life. At least, it was, until you 
+# notice that something's wrong with the grid of lights you bought: four 
+# lights, one in each corner, are stuck on and can't be turned off. The example
+# above will actually run like this:
+#
+# Initial state:
+#
+# ##.#.#
+# ...##.
+# #....#
+# ..#...
+# #.#..#
+# ####.#
+#
+# After 1 step:
+#
+# #.##.#
+# ####.#
+# ...##.
+# ......
+# #...#.
+# #.####
+#
+# After 2 steps:
+#
+# #..#.#
+# #....#
+# .#.##.
+# ...##.
+# .#..##
+# ##.###
+#
+# After 3 steps:
+#
+# #...##
+# ####.#
+# ..##.#
+# ......
+# ##....
+# ####.#
+#
+# After 4 steps:
+#
+# #.####
+# #....#
+# ...#..
+# .##...
+# #.....
+# #.#..#
+#
+# After 5 steps:
+#
+# ##.###
+# .##..#
+# .##...
+# .##...
+# #.#...
+# ##...#
+#
+# After 5 steps, this example now has 17 lights on.
+#
+# In your grid of 100x100 lights, given your initial configuration, but with 
+# the four corners always in the on state, how many lights are on after 100 
+# steps?
+#
 # ----------------------------------------------------------------------------
 
 import re
@@ -211,7 +280,6 @@ if __name__ == '__main__':
     day18 = Day18(data)
     print('Part 1:', day18.steps(100))
     # Correct answer is 1061
-
 
     data2 = open('day18_2.txt').read()
     day18_2 = Day18(data2, True)
