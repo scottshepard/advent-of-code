837b6277b8fffc50bf2cdc31162207c0eb7692a7
Scott Shepard <scott@shepardmail.com>
2016-12-10 21:47:23 -0600

    Grid and light classes

--- 2015/Day18/day18.py
+++ 2015/Day18/day18.py
@@ -88,19 +88,67 @@
 
 import re
 
-class Grid:
+class Light:
 
-    def __init__(self, grid):
-        self.grid = grid
+    def __init__(self, x, y, char):
+        self.char = char
+        self.on = self.on(char)
 
     def __repr__(self):
-        return ''.join([''.join(row) + '\n' for row in self.grid])
+        return self.char
+
+    def on(self, char):
+        if char == '#':
+            return True
+        else:
+            return False
+
+class Grid:
+
+    def __init__(self, n_cols, n_rows):
+        self.grid = [['' for x in range(n_cols)] for y in range(n_rows)]
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+
+    def __repr__(self):
+        grid = self.grid
+        return ''.join([''.join([l.char for l in row]) + '\n' for row in grid])
+
+    def __getitem__(self, i):
+        return self.grid[i]
+
+    def add_light(self, x, y, char):
+        self.grid[x][y] = Light(x, y, char)
+
+    def get_light(self, x, y):
+        if x == -1 or y == -1:
+            return
+        try:
+            return self.grid[y][x]
+        except:
+            pass
+
+    def sum_neighbors(self, x, y):
+        return sum([x.on for x in self.get_neighbors(x, y)])
+
+    def get_neighbors(self, x, y):
+        grid = self.grid
+        light = self.get_light(x, y)
+        neighbors = []
+        for j in range(y-1, y+2):
+            for i in range(x-1, x+2):
+                if (i, j) != (x, y):
+                    neighbors.append(self.get_light(i, j))
+        return [n for n in neighbors if n is not None]
 
 class Day18:
 
     def __init__(self, input_path):
         self.lines = re.split('\n', open(input_path).read())
-        self.grid = Grid([list(line) for line in self.lines])
+        self.grid = Grid(len(self.lines[0]), len(self.lines))
+        for j in range(self.grid.n_rows):
+            for i in range(self.grid.n_cols):
+                self.grid.add_light(i, j, self.lines[j][i])
 
 if __name__ == '__main__':
     day18 = Day18('day18_test.txt')
