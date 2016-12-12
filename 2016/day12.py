a94ce85f98897e04bde487602a03436fc3058cd7
Scott Shepard <scott@shepardmail.com>
2016-12-12 00:37:00 -0600

    Almost there! Stuck in a loop somewhere

--- 2016/day12.py
+++ 2016/day12.py
@@ -45,3 +45,88 @@
 # After executing the assembunny code in your puzzle input, what value is left in register a?
 #
 # -----------------------------------------------------------------------------
+
+import re
+
+class Register:
+
+    def __init__(self, name):
+        self.name = name
+        self.value = 0
+
+    def __str__(self):
+        return str({self.name: self.value})
+
+    def cpy(self, value):
+        self.value = value
+
+    def inc(self):
+        self.value += 1
+
+    def dec(self):
+        self.value -= 1
+
+class InstructionParser:
+
+    def __init__(self, instructions):
+        self.instructions = instructions
+        self.next_instruction_i = 0
+        self.registers = [Register(x) for x in list('abcd')]
+
+    def __repr__(self):
+        return str([str(x) for x in self.registers])
+
+    def parse_instructions(self, up_to):
+        index = 0
+        while index < up_to - 1:
+            print(index)
+            instruction = self.instructions[index]
+            index += self.parse_instruction(instruction)
+        return self
+
+    def parse_instruction(self, instruction):
+        method = re.search('cpy|inc|dec|jnz', instruction).group(0)
+        if method == 'cpy':
+            letter = re.search('[a-z]$', instruction).group(0)
+            register = self.register(letter)
+            value = re.search('-?[0-9]+|((?<= )[a-z](?= ))', instruction).group(0)
+            if value in list('abcd'):
+                register.cpy(self.register(value).value)
+            else:
+                register.cpy(int(value))
+            increment = 1
+        elif method == 'inc':
+            letter = re.search('[a-z]$', instruction).group(0)
+            register = self.register(letter)
+            register.inc()
+            increment = 1
+        elif method == 'dec':
+            letter = re.search('[a-z]$', instruction).group(0)
+            register = self.register(letter)
+            register.dec()
+            increment = 1
+        elif method == 'jnz':
+            letter = re.search('(?<=jnz )([a-z]|(-?[0-9]))', instruction).group(0)
+            if letter in list('abcd'):
+                value = self.register(letter).value
+            else:
+                value = int(letter)
+            increment = int(re.search('-?[0-9]+', instruction).group(0))
+            if value == 0:
+                increment = 1
+        print(instruction)
+        print(self)
+        return increment
+
+    def register(self, name):
+        return [r for r in self.registers if r.name == name][0]
+
+if __name__ == '__main__':
+    instructions = open('inputs/day12.txt').read().splitlines()
+    ip = InstructionParser(instructions)
+    # ip.parse_instructions(22)
+    
+
+
+
+
