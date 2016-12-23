4be31fd3da5bfd9c83901e28c7c3b7bb30b9576e
Scott Shepard <scott@shepardmail.com>
2016-12-22 21:26:23 -0600

    Unscramble strings of length 8

--- 2016/day21.py
+++ 2016/day21.py
@@ -14,6 +14,12 @@ class PasswordGenerator:
             self.parse_rule_scramble(rule)
         return self.pwd
 
+    def unscramble_password(self, pwd):
+        self.pwd = pwd
+        for rule in reversed(self.rules):
+            self.parse_rule_unscramble(rule)
+        return self.pwd
+
     def parse_rule_scramble(self, rule):
         rules_regex = 'swap position|swap letter|reverse|rotate based|rotate|move'
         search = re.search(rules_regex, rule).group(0)
@@ -40,6 +46,37 @@ class PasswordGenerator:
             pos1 = int(re.search('[0-9]+', rule).group(0))
             pos2 = int(re.search('[0-9]+$', rule).group(0))
             return self.move(pos1, pos2)
+
+    def parse_rule_unscramble(self, rule):
+        rules_regex = 'swap position|swap letter|reverse|rotate based|rotate|move'
+        search = re.search(rules_regex, rule).group(0)
+        if search == 'swap position':
+            pos1 = int(re.search('[0-9]+', rule).group(0))
+            pos2 = int(re.search('[0-9]+$', rule).group(0))
+            return self.swap_position(pos1, pos2)
+        elif search == 'swap letter':
+            letter1 = re.search('(?<= )[a-z](?= )', rule).group(0)
+            letter2 = re.search('(?<= )[a-z]$', rule).group(0)
+            return self.swap_letter(letter1, letter2)
+        elif search == 'reverse':
+            pos1 = int(re.search('[0-9]+', rule).group(0))
+            pos2 = int(re.search('[0-9]+$', rule).group(0))
+            return self.reverse(pos1, pos2)
+        elif search == 'rotate':
+            direction = re.search('left|right', rule).group(0)
+            steps = int(re.search('[0-9]+', rule).group(0))
+            if direction == 'left':
+                direction = 'right'
+            else:
+                direction = 'left'
+            return self.rotate_direction(direction, steps)
+        elif search == 'rotate based':
+            letter = re.search('(?<= )[a-z]$', rule).group(0)
+            return self.unrotate_letter(letter)
+        elif search == 'move':
+            pos1 = int(re.search('[0-9]+', rule).group(0))
+            pos2 = int(re.search('[0-9]+$', rule).group(0))
+            return self.move(pos2, pos1)
 
     def swap_position(self, pos1, pos2):
         chars = list(self.pwd)
@@ -85,6 +122,26 @@ class PasswordGenerator:
             extra = 0
         return self.rotate_direction('right', 1 + index + extra)
 
+    def unrotate_letter(self, letter):
+        len_ = len(self.pwd)
+        index = self.pwd.index(letter)
+        if index == 1:
+            return self.rotate_direction('left', 1)
+        if index == 3:
+            return self.rotate_direction('left', 2)
+        if index == 5:
+            return self.rotate_direction('left', 3)
+        if index == 7:
+            return self.rotate_direction('left', 4)
+        if index == 2:
+            return self.rotate_direction('left', 6)
+        if index == 4:
+            return self.rotate_direction('left', 7)
+        if index == 6:
+            return self.rotate_direction('left', 8)
+        if index == 0:
+            return self.rotate_direction('left', 9)
+
     def move(self, pos1, pos2):
         chars = list(self.pwd)
         char1 = chars[pos1]
@@ -102,3 +159,4 @@ if __name__ == '__main__':
     pg = PasswordGenerator(rules)
     print('Part 1:', pg.scramble_password('abcdefgh'))
     
+    assert(pg.unscramble_password('hcdefbag') == 'abcdefgh')
