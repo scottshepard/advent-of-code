db7f88a55ec5d95b53ac62f7f9418b2c9e1f2125
Scott Shepard <scott@shepardmail.com>
2016-12-23 02:04:04 -0500

    Rewrite of main parsing function and addition of toggle function. Now the original jnz function doens't work in all cases :(

--- 2016/day23.py
+++ 2016/day23.py
@@ -18,7 +18,7 @@ class Register:
     def dec(self):
         self.value -= 1
 
-class InstructionParser:
+class Interpreter:
 
     def __init__(self, instructions):
         self.instructions = instructions
@@ -28,53 +28,86 @@ class InstructionParser:
     def __repr__(self):
         return str([str(x) for x in self.registers])
 
-    def tgl(self, register):
+    def tgl(self, instruction):
+        value = re.search('-?[0-9a-z]+$', instruction).group(0)
+        if value in 'abcd':
+            value = self.register(value).value
+        else:
+            value = int(value)
+        instruction2 = self.instructions[self.index+value]
+        method = re.search('cpy|inc|dec|jnz|tgl', instruction2).group(0)
+        if method == 'inc':
+            instruction2 = re.sub('inc', 'dec', instruction2)
+        elif method in ['dec', 'tgl']:
+            instruction2 = re.sub('tgl|dec', 'inc', instruction2)
+        elif method == 'jnz':
+            instruction2 = re.sub('jnz', 'cpy', instruction2)
+        elif method == 'cpy':
+            instruction2 = re.sub('cpy', 'jnz', instruction2)
+        self.instructions[self.index + value] = instruction2
+        return 1
 
-    def parse_instructions(self, up_to):
-        index = 0
-        move = 0
-        while index < up_to:
-            if move % 500000 == 0:
-                print('Move:', move, self, end='\r')
-            instruction = self.instructions[index]
-            index += self.parse_instruction(instruction)
-            move += 1
+    def cpy(self, instruction):
+        letter = re.search('[a-z]$', instruction).group(0)
+        register = self.register(letter)
+        value = re.search('-?[0-9]+|((?<= )[a-z](?= ))', instruction).group(0)
+        if value in list('abcd'):
+            register.cpy(self.register(value).value)
+        else:
+            register.cpy(int(value))
+        return 1
+
+    def inc(self, instruction):
+        letter = re.search('[a-z]$', instruction).group(0)
+        register = self.register(letter)
+        register.inc()
+        return 1
+
+    def dec(self, instruction):
+        letter = re.search('[a-z]$', instruction).group(0)
+        register = self.register(letter)
+        register.dec()
+        return 1
+
+    def jnz(self, instruction):
+        letter = re.search('(?<=jnz )([a-z]|(-?[0-9]))', instruction).group(0)
+        if letter in list('abcd'):
+            value = self.register(letter).value
+        else:
+            value = int(letter)
+        increment = int(re.search('-?[0-9]+$', instruction).group(0))
+        if value == 0:
+            increment = 1
+        return increment
+
+    def solve(self, up_to=None, print_ = False):
+        if up_to is None:
+            up_to = len(self.instructions)
+        self.index = 0
+        self.move = 0
+        while self.index < up_to:
+            if print_:
+                print('Move:', self.move, self, end='\n')
+            instruction = self.instructions[self.index]
+            self.index += self.parse_instruction(instruction)
+            self.move += 1
 
     def parse_instruction(self, instruction):
         method = re.search('cpy|inc|dec|jnz|tgl', instruction).group(0)
         if method == 'tgl':
+            return self.tgl(instruction)
         elif method == 'cpy':
-            letter = re.search('[a-z]$', instruction).group(0)
-            register = self.register(letter)
-            value = re.search('-?[0-9]+|((?<= )[a-z](?= ))', instruction).group(0)
-            if value in list('abcd'):
-                register.cpy(self.register(value).value)
-            else:
-                register.cpy(int(value))
-            increment = 1
+            return self.cpy(instruction)
         elif method == 'inc':
-            letter = re.search('[a-z]$', instruction).group(0)
-            register = self.register(letter)
-            register.inc()
-            increment = 1
+            return self.inc(instruction)
         elif method == 'dec':
-            letter = re.search('[a-z]$', instruction).group(0)
-            register = self.register(letter)
-            register.dec()
-            increment = 1
+            return self.dec(instruction)
         elif method == 'jnz':
-            letter = re.search('(?<=jnz )([a-z]|(-?[0-9]))', instruction).group(0)
-            if letter in list('abcd'):
-                value = self.register(letter).value
-            else:
-                value = int(letter)
-            increment = int(re.search('-?[0-9]+$', instruction).group(0))
-            if value == 0:
-                increment = 1
-        return increment
+            return self.jnz(instruction)
 
     def register(self, name):
         return [r for r in self.registers if r.name == name][0]
 
 if __name__ == '__main__':
-    lines = open('inputs/day23.txt').read().splitlines()
+    instructions = open('inputs/day23_test.txt').read().splitlines()
+    interp = Interpreter(instructions)
