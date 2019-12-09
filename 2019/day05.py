345432b6d256f2e7a31784101cf20c3171aa1a6e
Scott Shepard <scott@shepardmail.com>
2019-12-09 16:38:47 -0600

    It should work now but definitely doesn't

--- 2019/day05.py
+++ 2019/day05.py
@@ -5,16 +5,16 @@ import pandas as pd
 import pdb
 
 
-class Intcode:
+class IntcodeComputer:
 
-    def __init__(self, codes):
-        self.codes_raw = codes
-        self.codes = copy.deepcopy(codes)
+    def __init__(self, input):
+        self.input = copy.deepcopy(input)
+        self.codes = [int(x) for x in input.split(',')]
         self.pos = 0
         self.solved = False
 
     def reset(self):
-        self.codes = copy.deepcopy(self.codes_raw)
+        self.codes = copy.deepcopy(self.input)
         self.solved = False
         self.pos = 0
 
@@ -26,9 +26,9 @@ class Intcode:
         instr = str(instr)
         opcode = int(instr[-2:])
         if opcode == 1 or opcode == 2:
-            return [int(d) for d in list('0' * (4 - len(str(opcode))) + instr[:-2])], opcode
+            return [int(d) for d in list('0' * (5 - len(instr)) + instr[:-2])], opcode
         elif opcode == 3 or opcode == 4:
-            return [int(d) for d in list('0' * (2 - len(str(opcode))) + instr[:-2])], opcode
+            return [int(d) for d in list('0' * (2 - len(instr)) + instr[:-2])], opcode
         elif opcode == 99:
             return [], opcode
 
@@ -38,34 +38,48 @@ class Intcode:
         elif parameter_mode == 1:
             return parameter
 
-    def compute_step(self, input):
+    def compute_step(self, input=None):
         codes = self.codes
         pos = self.pos
         param_modes, opcode = self.parameter_modes(codes[pos])
-        parameters = self.codes[pos:(pos+len(param_modes))]
+        parameters = self.codes[(pos+1):(pos+1+len(param_modes))]
         if opcode == 99:
             self.solved = True
-            return
         elif opcode == 1:
+            values = []
             for p, pm in zip(parameters, param_modes):
-
-            codes[codes[pos+3]] = codes[codes[pos+1]] + codes[codes[pos+2]]
+                values.append(self.fetch_param(p, pm))
+            codes[codes[pos+3]] = values[0] + values[1]
             self.pos += 4
         elif opcode == 2:
-            codes[codes[pos+3]] = codes[codes[pos+1]] * codes[codes[pos+2]]
+            values = []
+            for p, pm in zip(parameters, param_modes):
+                values.append(self.fetch_param(p, pm))
+            codes[codes[pos+3]] = values[0] * values[1]
             self.pos += 4
         elif opcode == 3:
             codes[opcode[pos+1]] = codes[input]
             self.pos += 2
         elif opcode == 4:
+            value = self.fetch_param(parameters[0], param_modes[0])
+            self.pos += 2
+            return value
 
-
-
-        self.opcode = opcode
 
 if __name__ == '__main__':
-    input = aoc.read_input('day05.txt')
-    intcode = Intcode(input)
+    input = aoc.read_input('day05.txt')[0]
+    ic = IntcodeComputer(input)
+    assert(ic.parameter_modes(1002) == ([0, 1, 0], 2))
+    assert(ic.parameter_modes(1101) == ([0, 1, 1], 1))
+    assert(ic.parameter_modes(3) == ([0], 3))
+
+    '1101, 100, -1, 4, 0'
+
+    param_modes, opcode = ic.parameter_modes(ic.codes[ic.pos])
+    parameters = ic.codes[(ic.pos+1):(ic.pos+1+len(param_modes))]
+
+    test_input = '3,0,4,0,99'
+    ic_test = IntcodeComputer(test_input)
 
 
 
