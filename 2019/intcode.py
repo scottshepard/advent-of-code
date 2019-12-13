897f04785a1890e628041163a563a65c7a658d3e
Scott Shepard <scott@shepardmail.com>
2019-12-12 21:58:02 -0600

    uncomment out test functions

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -343,26 +343,30 @@ class TestIntcodeComputer:
         assert ac3.thruster_signal(0) == 65210
 
         # Day 7 part 1 solution
-        ac4 = AmplifierChain(read_input('day07.txt'), [2,3,0,4,1])
+        ac4 = AmplifierChain(read_input('day07.txt')[0], [2,3,0,4,1])
         assert ac4.thruster_signal(0) == 24405
+
+
+        txt = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
+        ac5 = AmplifierChain(txt, [9, 8, 7, 6, 5])
+        assert ac5.thruster_signal2(0) == 139629729
+
+        txt = '''
+        3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
+        '''
+        ac6 = AmplifierChain(txt, [9, 7, 8, 5, 6])
+        assert ac6.thruster_signal2(0) == 18216
 
 
 if __name__ == '__main__':
     import pdb
 
-    #TestIntcodeComputer.test_opcodes_1_2()
-    #TestIntcodeComputer.test_opcodes_3_4()
-    #TestIntcodeComputer.test_opcodes_5_6()
-    #TestIntcodeComputer.test_opcodes_7_8()
-    #TestIntcodeComputer.test_amplifier_chain()
+    TestIntcodeComputer.test_opcodes_1_2()
+    TestIntcodeComputer.test_opcodes_3_4()
+    TestIntcodeComputer.test_opcodes_5_6()
+    TestIntcodeComputer.test_opcodes_7_8()
+    TestIntcodeComputer.test_amplifier_chain()
 
-    ac5 = AmplifierChain('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5', [9,8,7,6,5])
-    assert ac5.thruster_signal2(0) == 139629729
 
-    txt = '''
-    3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
-    -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
-    53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
-    '''
-    ac6 = AmplifierChain(txt, [9,7,8,5,6])
-    assert ac6.thruster_signal2(0) == 18216
