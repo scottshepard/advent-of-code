80ee7860826576a646f8ab687b880595920e7190
Scott Shepard <scott@shepardmail.com>
2019-12-13 09:35:18 -0600

    Part 1 formatting

--- 2019/day09.py
+++ 2019/day09.py
@@ -5,4 +5,4 @@ from intcode import IntcodeComputer
 input = read_input('day09.txt')[0]
 ic = IntcodeComputer(input)
 ic.next([1])
-print(ic.outputs)
+print('Solution to Day 9 Part I is {}'.format(ic.outputs[-1]))
