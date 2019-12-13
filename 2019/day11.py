e5ed0894d3958ba3ef5001ea48028d98103874e0
Scott Shepard <scott@shepardmail.com>
2019-12-13 16:48:59 -0600

    Part II whoo

--- 2019/day11.py
+++ 2019/day11.py
@@ -2,13 +2,16 @@ from utils import read_input
 from intcode import IntcodeComputer
 import copy
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 class HullPaintingRobot:
 
-    def __init__(self, input):
+    def __init__(self, input, starting_panel):
         self.input_raw = copy.deepcopy(input)
         self.brain = IntcodeComputer(input)
         self.direction = 'UP'
-        self.hull = {}
+        self.hull = {(0, 0): starting_panel}
         self.x = 0
         self.y = 0
         self.solved = False
@@ -66,11 +69,30 @@ class HullPaintingRobot:
             self.x += 1
 
 
-
 input = read_input('day11.txt')[0]
-robot = HullPaintingRobot(input)
 
+robot = HullPaintingRobot(input, starting_panel = 0)
 while not robot.solved:
     robot.next()
 
-print("Solutio to Day 11 Part I is {}".format(len(list(robot.hull.values()))))
+print("Solutiom to Day 11 Part I is {}".format(len(list(robot.hull.values()))))
+
+robot = HullPaintingRobot(input, starting_panel = 1)exi
+while not robot.solved:
+    robot.next()
+
+# Write output imge
+x_s = [k[0] for k in robot.hull.keys()]
+y_s = [k[1] for k in robot.hull.keys()]
+min_y = min(y_s)
+min_x = min(x_s)
+
+hull_shape = (max(y_s) - min(y_s) + 1, max(x_s) - min(x_s) + 1)
+arr = np.zeros(hull_shape)
+
+for k, v in robot.hull.items():
+    arr[k[1] - min_y][k[0] - min_x] = v
+arr = np.flip(arr, 0)
+plt.imshow(arr, interpolation='nearest')
+plt.savefig('day11_part2.png')
+print('Solution to Day 11 Part II is in day11_part2.png')
