f1e2d7e4251272f7f948454047a8944d30faa147
Scott Shepard <scott@shepardmail.com>
2019-12-12 21:52:46 -0600

    Holy shit I got part II

--- 2019/day07.py
+++ 2019/day07.py
@@ -6,12 +6,15 @@ import pdb
 
 from intcode import IntcodeComputer, AmplifierChain
 
-def find_max_thruster_signal(input_code, signal_digits=[0,1,2,3,4]):
+def find_max_thruster_signal(input_code, signal_digits=[0,1,2,3,4], part=1):
     thruster_signals = []
     perms = permutations(signal_digits)
     for perm in list(perms):
         ac = AmplifierChain(input_code, perm)
-        thruster_signals.append(ac.thruster_signal(0))
+        if part == 1:
+            thruster_signals.append(ac.thruster_signal(0))
+        elif part == 2:
+            thruster_signals.append(ac.thruster_signal2(0))
     return pd.Series(thruster_signals).max()
 
 
@@ -21,6 +24,4 @@ if __name__ == '__main__':
     print('Solution to Day 7 Part I is {}'.format(find_max_thruster_signal(acs)))
 
     # Part 2
-    #acs_test2 = aoc.read_input('day07_test2.txt')
-    #ac2 = AmplifierChain(acs_test2[0], phase_setting=[9,8,7,6,5])
-    #ac2.thruster_signal2(0)
+    print('Solution to Day 7 Part II is {}'.format(find_max_thruster_signal(acs, [5,6,7,8,9], 2)))
