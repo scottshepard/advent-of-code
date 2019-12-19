d55ad95fd88b1948cb8db677f3a195c25bcb7ffe
Scott Shepard <scott@shepardmail.com>
2019-12-18 23:00:05 -0600

    Part II

--- 2019/day13.py
+++ 2019/day13.py
@@ -14,14 +14,26 @@ class Breakout:
         4: 'o'
     }
 
-    def __init__(self, input):
+    def __init__(self, input, q=None):
         self.input_raw = deepcopy(input)
         self.IC = IntcodeComputer(input)
+        if q is not None:
+            self.IC.source_code[0] = q
         self.screen = np.ndarray((24, 42), dtype=np.dtype('b'))
+        self.next_screen(0)
+        self.score = 0
 
-    def next(self, i):
+    def play(self):
+        while not self.game_over:
+            self.next()
+
+    def next(self):
+        i = self.next_input()
+        self.next_screen(i)
+
+    def next_screen(self, i):
         self.IC.halt = False
-        self.IC.next([i])
+        _, self.game_over = self.IC.next([i])
         n_squares = int(len(self.IC.outputs) / 3)
         squares = np.reshape(self.IC.outputs, (n_squares, 3))
         for square in squares:
@@ -29,7 +41,19 @@ class Breakout:
             if x==-1 and y==0:
                 self.score = c
             else:
+                if c == 3:
+                    self.x_paddle = x
+                elif c == 4:
+                    self.x_ball = x
                 self.screen[y][x] = c
+
+    def next_input(self):
+        if self.x_paddle > self.x_ball:
+            return -1
+        elif self.x_paddle < self.x_ball:
+            return 1
+        else:
+            return 0
 
     def __repr__(self):
         out = ''
@@ -42,18 +66,11 @@ class Breakout:
 
 
 
-
-
 input = read_input('day13.txt')[0]
 
-#breakout = Breakout(input)
-#breakout.next(0)
-#print('Solution to Day 13 part I is {}'.format((breakout.screen == 2).sum()))
-
 breakout = Breakout(input)
-breakout.IC.source_code[0] = 2
-breakout.next(0)
-print(breakout)
+print('Solution to Day 13 part I is {}'.format((breakout.screen == 2).sum()))
 
-
-
+breakout = Breakout(input, 2)
+breakout.play()
+print('Solution to Day 13 part II is {}'.format(breakout.score))
