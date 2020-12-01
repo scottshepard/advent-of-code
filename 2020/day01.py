58594ae95616f643aa0eca305803e3397b89ae18
Scott Shepard <scott@shepardmail.com>
2020-12-01 10:07:01 -0500

    Day 1, 2020 - Part 1

--- 2020/day01.py
+++ 2020/day01.py
@@ -4,3 +4,19 @@ from utils import read_input
 
 
 input = read_input('day01.txt')
+
+
+def sum_to(num, lst):
+    '''
+    :param num: Number to sum to
+    :param lst: List of ints
+    :return: Two elements in the lst that sum to the num
+    '''
+    lst = [int(x) for x in lst]
+    for i in range(len(lst)):
+        for j in range(len(lst)-i):
+            if lst[i] + lst[j] == num:
+                return lst[i], lst[j]
+
+x, y = sum_to(2020, input)
+print('Part 1:', x * y)
