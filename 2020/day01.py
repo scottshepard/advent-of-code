1ac3f8d017a8de84b1c61277bc7e487596d47d3d
Scott Shepard <scott@shepardmail.com>
2020-12-01 10:49:36 -0500

    Day 1, 2020 - part 2

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,12 +1,10 @@
-from math import floor
-import numpy as np
 from utils import read_input
 
 
 input = read_input('day01.txt')
 
 
-def sum_to(num, lst):
+def sum_to(num, lst, n=2):
     '''
     :param num: Number to sum to
     :param lst: List of ints
@@ -14,9 +12,17 @@ def sum_to(num, lst):
     '''
     lst = [int(x) for x in lst]
     for i in range(len(lst)):
-        for j in range(len(lst)-i):
-            if lst[i] + lst[j] == num:
-                return lst[i], lst[j]
+        for j in range(i, len(lst)):
+            if n == 2:
+                if lst[i] + lst[j] == num:
+                    return lst[i], lst[j]
+            if n == 3:
+                for k in range(j, len(lst)):
+                    if lst[i] + lst[j] + lst[k] == num:
+                        return lst[i], lst[j], lst[k]
 
 x, y = sum_to(2020, input)
 print('Part 1:', x * y)
+
+x, y, z = sum_to(2020, input, 3)
+print('Part 2:', x * y * z)
