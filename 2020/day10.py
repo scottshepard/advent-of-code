f1732259957c3e34e9e025ab0bc04467313da8d8
Scott Shepard <scott@shepardmail.com>
2020-12-13 21:37:46 -0500

    Day 10, 2020 - Part 2

--- 2020/day10.py
+++ 2020/day10.py
@@ -6,7 +6,7 @@ import pdb
 
 sample_input = read_input('day10_test.txt')
 
-def solve_part_1(input):
+def count_diffs(input):
     lst = [int(x) for x in input]
     lst.insert(0, 0)
     lst.append(max(lst)+3)
@@ -23,9 +23,16 @@ def solve_part_1(input):
                 break
             else:
                 j += 1
-    a = np.array(diffs)
-    unique, counts = np.unique(a, return_counts=True)
-    jolts = dict(zip(unique, counts))
+    return diffs
+
+def value_counts(lst):
+    unique, counts = np.unique(lst, return_counts=True)
+    return dict(zip(unique, counts))
+
+
+def solve_part_1(input):
+    diffs = count_diffs(input)
+    jolts = value_counts(diffs)
     return jolts[1] * jolts[3]
 
 assert solve_part_1(sample_input) == 35
@@ -35,3 +42,27 @@ assert solve_part_1(sample_input2) == 220
 
 real_input = read_input('day10.txt')
 print('Part 1:', solve_part_1(real_input))
+
+def solve_part_2(input):
+    diffs = count_diffs(input)
+    diffs.insert(0, 3)
+    pattern = []
+    for i, d in enumerate(diffs):
+        if d == 1:
+            continue
+        else:
+            count_1s = 0
+            j = 1
+            while (i+j < len(diffs) and (diffs[i+j] == 1)):
+                j += 1
+                count_1s += 1
+            pattern.append(count_1s)
+    P = value_counts(pattern)
+    if 4 not in P.keys():
+        P[4] = 0
+    return 2**P[2] * 4**P[3] * 7**P[4]
+
+assert solve_part_2(sample_input) == 8
+assert solve_part_2(sample_input2) == 19208
+
+print('Part 2:', solve_part_2(real_input))
