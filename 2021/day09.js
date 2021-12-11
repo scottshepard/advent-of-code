af7fa64002a104320fe9c0204242ea1085a7b83c
Scott Shepard <scott@macbook.myfiosgateway.com>
2021-12-10 22:18:37 -0500

    Day 09, 2021 - Part 2

--- 2021/day09.js
+++ 2021/day09.js
@@ -20,7 +20,8 @@ function getSurrondingPoints(x, y) {
    return results 
 }
 
-var result = 0
+var part1_score = 0
+var lowPoints = []
 for(var j=0; j < data.length; j++) {
     var row = data[j]
     for(var i=0; i < row.length; i++) {
@@ -34,8 +35,38 @@ for(var j=0; j < data.length; j++) {
             }
         }
         if (higher_than == 0) {
-            result += point + 1
+            part1_score += point + 1
+            lowPoints.push([i,j])
         }
     }
 }
-console.log(result)
+console.log('Part 1:', part1_score)
+
+function searchBasin(x, y) {
+    var point = data[y][x]
+    if (data[y][x] == 9)
+        return
+    var basinPoints = [String([x,y])]
+    var compare = getSurrondingPoints(x, y)
+    for(var k=0; k < compare.length; k++) {
+        c = compare[k]
+        if (point < data[c[1]][c[0]]) {
+            var newBasinPoints = searchBasin(c[0], c[1])
+            if (newBasinPoints) {
+                for(var l=0; l < newBasinPoints.length; l++) {
+                    if (!(basinPoints.includes(newBasinPoints[l])))
+                        basinPoints.push(String(newBasinPoints[l]))
+                }
+            }
+        }
+    }
+    return basinPoints
+}
+
+var basins = []
+for(var i=0; i < lowPoints.length; i++) {
+    var lp = lowPoints[i]
+    basins.push(searchBasin(lp[0], lp[1]).length)
+}
+topBasins = basins.sort(function(a, b) {return b-a}).slice(0,3)
+console.log('Part 2:', topBasins.reduce((a,b) => a*b))
